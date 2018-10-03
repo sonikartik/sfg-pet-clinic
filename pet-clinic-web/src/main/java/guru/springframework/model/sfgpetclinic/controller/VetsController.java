d4cb0982deeb009a623a2913399b0628784177cc
kartik <soni.kartik@gmail.com>
2018-10-03 06:29:24 -0500

    Implement Vets Index Page And VetsController. Closes #14.

---
+++ pet-clinic-web/src/main/java/guru/springframework/model/sfgpetclinic/controller/VetsController.java
@@ -0,0 +1,12 @@
+package guru.springframework.model.sfgpetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetsController {
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets() {
+        return "vets/index";
+    }
+}
