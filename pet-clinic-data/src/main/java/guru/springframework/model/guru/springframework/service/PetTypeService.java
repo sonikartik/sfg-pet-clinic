d9a47ef7fbc8ac0a60979dc277fa13ff3603a542
kartik <soni.kartik@gmail.com>
2018-10-04 11:58:01 -0500

    Closes #28.

---
+++ pet-clinic-data/src/main/java/guru/springframework/model/guru/springframework/service/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.model.guru.springframework.service;
+
+import guru.springframework.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/model/guru/springframework/service/map/PetTypeMapServiceImpl.java
@@ -0,0 +1,36 @@
+package guru.springframework.model.guru.springframework.service.map;
+
+import guru.springframework.model.PetType;
+import guru.springframework.model.guru.springframework.service.CrudService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapServiceImpl extends AbstractMapService<PetType, Long> implements CrudService<PetType, Long> {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType t) {
+        super.delete(t);
+    }
+
+    @Override
+    public PetType save(PetType t) {
+        return super.save(t);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
