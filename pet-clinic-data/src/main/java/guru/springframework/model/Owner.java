6b1d22f9d99b1e9a9738adf66f0f61d9577ac9a6
kartik <soni.kartik@gmail.com>
2018-10-12 18:38:08 -0500

    Closes #37.

--- pet-clinic-data/src/main/java/guru/springframework/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/model/Owner.java
@@ -7,12 +7,16 @@ import java.util.Set;
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
+
     @Column(name = "address")
     private String address;
+
     @Column(name = "city")
     private String city;
+
     @Column(name = "telephone")
     private String telephone;
+
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
@@ -39,6 +43,7 @@ public class Owner extends Person {
     public void setTelephone(String telephone) {
         this.telephone = telephone;
     }
+
     public Set<Pet> getPets() {
         return pets;
     }
@@ -46,5 +51,4 @@ public class Owner extends Person {
     public void setPets(Set<Pet> pets) {
         this.pets = pets;
     }
-
 }
--- pet-clinic-data/src/main/java/guru/springframework/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/model/Pet.java
@@ -1,16 +1,31 @@
 package guru.springframework.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets ")
 public class Pet extends BaseEntity {
+
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+
+    Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -42,5 +57,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
